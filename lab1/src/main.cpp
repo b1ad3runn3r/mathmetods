4fb9b2e3a5b54d2d4cf2fba5822f18035dd0d20f
b1ad3runn3r <31962327+b1ad3runn3r@users.noreply.github.com>
2024-10-03 16:57:47 +0300

    lab1: implement compressed product

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <boost/dynamic_bitset/dynamic_bitset.hpp>
 #include "storage.hpp"
+#include "utils.hpp"
+#include <chrono>
 
-int main() {
-    storage::Storage storage_15(15);
+#include "word_splitting.hpp"
 
-    std::cout << storage_15.get_value(358, 437) << std::endl;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        return 1;
+    }
 
+    const auto power = std::strtoull(argv[1], nullptr, 10);
+    auto vec_size = static_cast<size_t>(std::pow(2, power));
+    boost::dynamic_bitset<uint64_t> test1(vec_size);
+    boost::dynamic_bitset<uint64_t> test2(vec_size);
+    test1 = utils::generate_random_bitset<uint64_t>(test1);
+    test2 = utils::generate_random_bitset<uint64_t>(test2);
 
+    bool res_1 = false;
+    auto start_1 = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < vec_size; ++i) {
+        res_1 ^= (test1[i] & test2[i]);
+    }
+    auto end_1 = std::chrono::high_resolution_clock::now();
+    auto delta1 = std::chrono::duration_cast<std::chrono::microseconds>(end_1 - start_1).count();
+    std::cout << res_1 << " Usual took " << delta1 << " ms" << std::endl;
 
+    auto start_2 = std::chrono::high_resolution_clock::now();
+    bool res_2 = word_splitting::multiply(test1, test2);
+    auto end_2 = std::chrono::high_resolution_clock::now();
+    auto delta2 = std::chrono::duration_cast<std::chrono::microseconds>(end_2 - start_2).count();
+    std::cout << "Optimized took " << delta2 << " ms" << std::endl;
+
+    assert(res_1 == res_2);
     return 0;
 }
---
+++ lab1/src/word_splitting.hpp
@@ -0,0 +1,10 @@
+#ifndef WORD_SPLITTING_HPP
+#define WORD_SPLITTING_HPP
+
+#include <boost/dynamic_bitset/dynamic_bitset.hpp>
+
+namespace word_splitting {
+    bool multiply(boost::dynamic_bitset<> &a, boost::dynamic_bitset<> &b);
+}
+
+#endif // WORD_SPLITTING_HPP
